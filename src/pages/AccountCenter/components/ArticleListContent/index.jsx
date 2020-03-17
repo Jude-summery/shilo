d436bd94ac65860437cb86cd31fd0df5d1288ede
Jude-summery <329543240@qq.com>
2020-03-18 00:03:38 +0800

    update：登录页背景图片，修改文档展示页面

--- src/pages/AccountCenter/components/ArticleListContent/index.jsx
+++ src/pages/AccountCenter/components/ArticleListContent/index.jsx
@@ -5,18 +5,10 @@ import styles from './index.less';
 import BraftEditor from 'braft-editor'
 import 'braft-editor/dist/output.css'
 
-function isJsonString(str) {
-  try {
-    console.log(JSON.parse(JSON.parse(str)), 56565656565656 , str)
-    if (typeof JSON.parse(str) === 'object') { return true }
-  } catch (e) { return false }
-  return false
-}
-
 const ArticleListContent = ({ data: { content, created_at, author, href } }) => {
   return (
     <div className={styles.listContent}>
-      {isJsonString(content) ? <div className="braft-output-content" dangerouslySetInnerHTML={{ __html: BraftEditor.createEditorState(JSON.parse(content)).toHTML() }}></div> : <div className={styles.description}>{content}</div>}
+      <div className="braft-output-content" dangerouslySetInnerHTML={{ __html: BraftEditor.createEditorState(content).toHTML() }}></div>
       <div className={styles.extra}>
         <Avatar src={`/api/user/avatar/get?imgid=${author.avatar}`} size="small" />
         <a href={href}>{author.nickname}</a> 发布于<em>{moment(created_at).format('YYYY-MM-DD HH:mm')}</em>
