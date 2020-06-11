a48a33bf3be105a2465aaa75189042441f8d08e5
xiazz <xiazz@unitechs.com>
2020-06-11 18:04:43 +0800

    feat：文档列表最大高度

--- src/pages/AccountCenter/components/ArticleListContent/index.jsx
+++ src/pages/AccountCenter/components/ArticleListContent/index.jsx
@@ -9,7 +9,8 @@ import xss from 'xss'
 const ArticleListContent = ({ data: { content, created_at, author, href } }) => {
   return (
     <div className={styles.listContent}>
-      <div className="braft-output-content" dangerouslySetInnerHTML={{ __html: xss(BraftEditor.createEditorState(content).toHTML()) }}></div>
+      <div className={styles.braftOutputContent} dangerouslySetInnerHTML={{ __html: xss(BraftEditor.createEditorState(content).toHTML()) }}></div>
+      <p>...</p>
       <div className={styles.extra}>
         <Avatar src={`/api/user/avatar/get?imgid=${author.avatar}`} size="small" />
         <a href={href}>{author.nickname}</a> 发布于<em>{moment(created_at).format('YYYY-MM-DD HH:mm')}</em>
