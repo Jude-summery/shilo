3599406bd40a3bd42df2751ce8c5df52c8999812
xiazz <xiazz@unitechs.com>
2020-06-11 13:59:35 +0800

    update：防xss

--- src/pages/AccountCenter/components/ArticleListContent/index.jsx
+++ src/pages/AccountCenter/components/ArticleListContent/index.jsx
@@ -1,14 +1,15 @@
-import { Avatar } from 'antd';
-import React from 'react';
-import moment from 'moment';
-import styles from './index.less';
+import { Avatar } from 'antd'
+import React from 'react'
+import moment from 'moment'
+import styles from './index.less'
 import BraftEditor from 'braft-editor'
 import 'braft-editor/dist/output.css'
+import xss from 'xss'
 
 const ArticleListContent = ({ data: { content, created_at, author, href } }) => {
   return (
     <div className={styles.listContent}>
-      <div className="braft-output-content" dangerouslySetInnerHTML={{ __html: BraftEditor.createEditorState(content).toHTML() }}></div>
+      <div className="braft-output-content" dangerouslySetInnerHTML={{ __html: xss(BraftEditor.createEditorState(content).toHTML()) }}></div>
       <div className={styles.extra}>
         <Avatar src={`/api/user/avatar/get?imgid=${author.avatar}`} size="small" />
         <a href={href}>{author.nickname}</a> 发布于<em>{moment(created_at).format('YYYY-MM-DD HH:mm')}</em>
